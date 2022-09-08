23fbdc2a7f9c38ef3c445acf72a1df3a348f43d1
Siddharth Amin <siddharth.amin17@gmail.com>
2022-09-09 00:51:22 +0200

    Update

--- page-script.js
+++ page-script.js
@@ -7,4 +7,23 @@ $(document).ready(function() {
 
 	//methods
 	$.fn.fullpage.setAllowScrolling(false);*/
+    const modelViewerVariants = document.querySelector("model-viewer#jersey");
+$('#jersey').css({'height': $('#slide-1').height(),'width': $('.jersey-container').width()});
+modelViewerVariants.addEventListener('load', () => {
+  const names = modelViewerVariants.availableVariants;
+  for (const name of names) {
+    //const option = document.createElement('option');
+    //option.value = name;
+    //option.textContent = name;
+    //select.appendChild(option);
+    console.log(name);
+    
+  }
+  modelViewerVariants.variantName = 'White';
+  // Adds a default option.
+  /*const option = document.createElement('option');
+    option.value = 'default';
+    option.textContent = 'Default';
+    select.appendChild(option);*/
+});
 });
