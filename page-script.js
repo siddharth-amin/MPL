17441c0daa10e6de8a421b28eb103c001f343b1b
Siddharth Amin <siddharth.amin17@gmail.com>
2022-09-09 02:14:32 +0200

    Progress & Story Animation

--- page-script.js
+++ page-script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
 	//methods
 	$.fn.fullpage.setAllowScrolling(false);*/
-    const modelViewerVariants = document.querySelector("model-viewer#jersey");
+    /*const modelViewerVariants = document.querySelector("model-viewer#jersey");
 $('#jersey').css({'height': $('.jersey-container').height(),'width': $('.jersey-container').width()});
 modelViewerVariants.addEventListener('load', () => {
   const names = modelViewerVariants.availableVariants;
@@ -24,6 +24,23 @@ modelViewerVariants.addEventListener('load', () => {
   /*const option = document.createElement('option');
     option.value = 'default';
     option.textContent = 'Default';
-    select.appendChild(option);*/
+    select.appendChild(option);*+/
+});*/
 });
-});
+
+function progressBar(){
+    progress = 0;
+    progCount = setInterval(() => {
+        $('.progress-tracker').html(progress >= 40 ? 40 + '%' : progress++ + '%');
+    }, 45);
+    $('.progress-bar-blue').animate({width: '40%'},2000, () =>{});
+    $('.progress-tracker').animate({left: '40%'},2000, () =>{
+        clearInterval(progCount);
+        openStoryPopup()
+    });
+}
+
+function openStoryPopup(){
+    $('.story-popup').addClass('animate__fadeInLeft');
+    
+}
